ab924e26d409f148f19237c37c174a36cc063bce
Beilak <beylak@yandex.ru>
2023-05-09 12:30:23 +0300

    add config type anotation

--- org/config.py
+++ org/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_user = "org_test_user"
-    db_pwd = "org_test_pwd"
-    db_host = "127.0.0.1"
-    db_port = "5000"
-    db_name = "org_test"
+    db_user: str
+    db_pwd: str
+    db_host: str
+    db_port: int
+    db_name: str
